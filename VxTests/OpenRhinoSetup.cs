374a336b3b26be6572a761c733c28d2d08cc2219
piac <giulio@mcneel.com>
2021-03-23 20:44:40 +0100

    change to Eto

--- VxTests/OpenRhinoSetup.cs
+++ VxTests/OpenRhinoSetup.cs
@@ -8,8 +8,6 @@ using System.Xml.Linq;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Diagnostics;
-using WinFormsApp;
-using System.Windows.Forms;
 
 namespace MxTests
 {
@@ -85,7 +83,6 @@ namespace MxTests
     }
 
     private Rhino.Runtime.InProcess.RhinoCore rhinoCore; //do NOT reference this by its RhinoCommon name
-    private MainForm mainform;
 
     [OneTimeSetUp, STAThread]
     public void OneTimeSetUp()
@@ -97,8 +94,21 @@ namespace MxTests
       else RhinoSystemDir = RhinoInside.Resolver.RhinoSystemDirectory;
       TestContext.WriteLine("RhinoSystemDir is: " + RhinoSystemDir + ".");
 
+      rhinoCore = new Rhino.Runtime.InProcess.RhinoCore(new string[] { "-appmode" }); //delayed as much as necessary
 
-      Application.Run(new MainForm());
+      MainForm mf = new MainForm();
+      mf.Show();
+
+      for (int i = 0; i < System.Windows.Forms.Application.OpenForms.Count; i++)
+      {
+        var window = System.Windows.Forms.Application.OpenForms[i];
+        if (window.Visible)
+        {
+          window.Closed += (s, e) => MainForm.Shutdown();
+          break;
+        }
+      }
+      System.Windows.Forms.Application.Run();
     }
 
     [OneTimeTearDown]
@@ -120,5 +130,75 @@ namespace MxTests
       Assert.IsTrue(File.Exists(OpenRhinoSetup.SettingsFile),
           $"File setting does not exist. Expected '{OpenRhinoSetup.SettingsFile}' in '{OpenRhinoSetup.SettingsDir}'.");
     }
+  }
+
+  class MainForm : Eto.Forms.Form
+  {
+    static Rhino.Runtime.InProcess.RhinoCore _rhinoCore;
+
+    internal static void Shutdown()
+    {
+      _rhinoCore.Dispose();
+      System.Windows.Forms.Application.Exit();
+    }
+
+    Rhino.UI.Controls.ViewportControl _viewportControl;
+    public MainForm()
+    {
+      Title = "Rhino.Inside";
+      ClientSize = new Eto.Drawing.Size(400, 400);
+      _viewportControl = new Rhino.UI.Controls.ViewportControl();
+      Content = _viewportControl;
+
+      var viewMenu = new Eto.Forms.ButtonMenuItem { Text = "&View" };
+      BuildDisplayModesMenu(viewMenu.Items);
+      Menu = new Eto.Forms.MenuBar()
+      {
+        Items =
+        {
+          new Eto.Forms.ButtonMenuItem
+          {
+            Text = "&File",
+            Items =
+            {
+              new Eto.Forms.ButtonMenuItem(new Eto.Forms.Command((s,e)=>OpenFile())) { Text = "Open..." }
+            }
+          },
+          viewMenu
+
+        }
+      };
+
+    }
+
+    void BuildDisplayModesMenu(Eto.Forms.MenuItemCollection collection)
+    {
+      Rhino.Display.DisplayModeDescription[] modes = Rhino.Display.DisplayModeDescription.GetDisplayModes();
+      foreach (var mode in modes)
+      {
+        var menuitem = new Eto.Forms.ButtonMenuItem((s, e) =>
+        {
+          _viewportControl.Viewport.DisplayMode = mode;
+          _viewportControl.Refresh();
+        });
+        menuitem.Text = mode.EnglishName;
+        collection.Add(menuitem);
+      }
+    }
+
+    void OpenFile()
+    {
+      var ofd = new Eto.Forms.OpenFileDialog();
+      ofd.Filters.Add(new Eto.Forms.FileFilter("Rhino 3dm", ".3dm"));
+      if (ofd.ShowDialog(this) == Eto.Forms.DialogResult.Ok)
+      {
+        Title = $"Rhino.Inside ({ofd.FileName})";
+        Rhino.RhinoDoc.Open(ofd.FileName, out bool alreadyOpen);
+        _viewportControl.Viewport.ZoomExtents();
+        _viewportControl.Refresh();
+      }
+    }
+
+
   }
 }
