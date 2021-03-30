680859b778325b2dadf50e1fb85fd689efd56fd2
piac <giulio@mcneel.com>
2021-03-30 20:10:30 +0200

    Interim update

--- VxTests/Display.cs
+++ VxTests/Display.cs
@@ -1,30 +1,47 @@
 using NUnit.Framework;
 using Rhino;
-using System;
 using System.IO;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace VxTests
 {
   [TestFixture]
   public class Display
   {
-    [Test, UIThread]
-    public void Regressions()
+    [Test, UIThread, TestCaseSource(nameof(GetTestModels))]
+    public void Regressions(string category, string test)
     {
-
-
-      var basep = OpenRhinoSetup.PathForTest(nameof(Display), nameof(Regressions));
+      var basep = OpenRhinoSetup.PathForTest(nameof(Display), category, test);
       var doc = RhinoDoc.Open(basep, out _);
 
       RhinoApp.Wait();
 
       var bitmap = doc.Views.First().DisplayPipeline.FrameBuffer;
 
-
+      bitmap.Save("");
       
       //Assert.Fail("Some text");
     }
-  }
 
+    public static IEnumerable<string[]> GetTestModels()
+    {
+      foreach(var root in VxContext.RootsFromHeading(nameof(Display)))
+      {
+        foreach(var dirs in VxContext.DirsFromRoot(root))
+        {
+          foreach (var test in VxContext.ModelsFromDirs(dirs))
+          {
+            yield return new string[] { test.Substring(root.Length), Path.GetFileNameWithoutExtension(test) };
+          }
+        }
+      }
+    }
+
+    public static string GetStorageFolder()
+    {
+      var path = Path.GetTempPath();
+      
+    }
+  }
 }
