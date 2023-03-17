ed4c6963f922c1f7fada99a22c18f12d57505894
Scott Davidson <scottd@mcneel.com>
2023-03-17 08:33:19 -0700

    Added the new external testing branch

---
+++ models/MeshBooleanUnion/area-union.py
@@ -0,0 +1,22 @@
+import rhinoscriptsyntax as rs
+
+ms = rs.GetObjects(filter=rs.filter.mesh)
+
+if ms:
+    
+    text = ["AREA UNION",""]
+    areas = []
+    for m in ms:
+        a = rs.Area(m)
+        areas.append(a)
+        
+    areas = sorted(areas)
+    
+    for a in areas:
+        text.append(repr(a))
+    
+    
+text = "\n".join(text)
+
+#print("OLD NOTES: ")
+print(rs.Notes(text))
