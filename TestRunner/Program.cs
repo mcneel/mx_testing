9551e7163448b16f825e6a72353e8ca8b5ebb9b3
piac <giulio@mcneel.com>
2023-08-14 20:02:25 +0200

    Move test runner location to standalone

--- TestRunner/Program.cs
+++ TestRunner/Program.cs
@@ -1,10 +1,22 @@
+using System;
 using System.Collections;
-
+using System.IO;
+using System.Reflection;
 
 class Program
 {
   static void Main()
   {
+    var this_location = Assembly.GetExecutingAssembly().Location;
+    var plug_dir = Path.GetDirectoryName(this_location);
+    var test_dir = Path.GetDirectoryName(plug_dir);
+
+    var test_dll = Path.Combine(test_dir, "MxTests.dll");
+    Assembly assembly = Assembly.LoadFrom(test_dll);
+
+    var type = assembly.GetType("MxTests.RhinoRunner.CommandTestRunner", true, false);
+    var testRunner = (ITestRunner)AppDomain.CurrentDomain.CreateInstanceAndUnwrap(assembly.FullName, type.FullName);
+
 
   }
 
