22e27891baecb71c04c68aaaf13d46b956b95b8c
Menno Deij - van Rijswijk <menno@mcneel.com>
2025-09-10 10:19:25 +0200

    Add nearly parallel line tests
    Cites RH-89164
    
    Also: fix MatchSrf.cs to get tests working in VS test explorer again.

--- NetSDKTests/Line.cs
+++ NetSDKTests/Line.cs
@@ -72,5 +72,27 @@ namespace NetSDKTests
 
       Assert.AreEqual(1.0, l0.MinimumDistanceTo(l1), RhinoMath.ZeroTolerance);
     }
+
+    [Test]
+    public void NearlyParallel()
+    {
+      Line A = new Line(5.4301839655138417, -9.5, 0, -0.6, -9.5, 0);
+      Line B = new Line(5.2373595635311068, 10.5, 0, 5.6603292194932395, 10.5, 0);
+
+      bool intersect = Rhino.Geometry.Intersect.Intersection.LineLine(A, B, out double a, out double b);
+      Assert.That(intersect, Is.False);
+    }
+
+    [Test]
+    public void RH89164_MinimumDistance()
+    {
+      Line A = new Line(-502.12241078825264, 463.17458426757145, -2.6515717396328807e-12, - 569.15428634054422, 1336.2761199492861, 0);
+      Line B = new Line(-474.85259053896368, 439.79346146347723, -2.7284841053187847e-12, -543.82881481395043, 1338.2204686719679, 0);
+
+      bool intersect = Rhino.Geometry.Intersect.Intersection.LineLine(A, B, out double a, out double b);
+      Assert.That(intersect, Is.False);
+
+      Assert.AreEqual(25.4, A.MinimumDistanceTo(B), RhinoMath.SqrtEpsilon);
+    }
   }
 }
